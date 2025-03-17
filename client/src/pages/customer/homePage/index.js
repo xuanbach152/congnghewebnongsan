4f2c37b5f39325d0fbd18a33088321fd0747b507
tranduyanhhtth <tranduyanhhtth@gmail.com>
2025-03-17 10:29:34 +0700

    init fe

---
+++ client/src/pages/customer/homePage/index.js
@@ -0,0 +1,6 @@
+import { memo } from "react";
+const HomePage = () => {
+    return <h1>HomePage</h1>;
+};
+
+export default memo(HomePage);
---
+++ client/src/pages/customer/profilePage/index.js
@@ -0,0 +1,6 @@
+import { memo } from "react";
+const ProfilePage = () => {
+    return <h1>ProfilePage</h1>;
+};
+
+export default memo(ProfilePage);
